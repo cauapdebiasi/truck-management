b5902e90c44eb0a5b6ffc7b767f1dcb6823490b1
Caua Debiasi <cauadebiasi@gmail.com>
2022-07-23 19:25:04 -0300

    fix!: changed base screen name to Base

--- src/screens/Base.tsx
+++ src/screens/Base.tsx
@@ -4,17 +4,17 @@ import Home from "./Home";
 import Dashboard from "./Dashboard";
 import Parts from "./Parts";
 import Settings from "./Settings";
-import styles from "@/assets/styles/screens/base.module.scss";
+import styles from "@/assets/styles/screens/Base.module.scss";
 
 const Base: React.FC = () => {
   return (
     <div className={styles.base}>
       <SidePanel></SidePanel>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/dashboard" element={<Dashboard />}/>
-        <Route path="/parts" element={<Parts />}/>
-        <Route path="/settings" element={<Settings />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/parts" element={<Parts />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </div>
   )
